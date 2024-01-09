1622570f7996dfc85e47439f93df1edbad7c8fe9
lifewawj <arevaloaj01@gmail.com>
2024-01-09 15:03:01 -0800

    Updated questions array and wrapped it within a function, Added a seeds.sql file to add data to the departments, role, and employee tables

--- db/schema.sql
+++ db/schema.sql
@@ -9,21 +9,29 @@ USE employee_db;
 
 -- Tables created for department, role, employees
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL
-    department_id INT
+    department_id INT,
+    salary DECIMAL,
+
+FOREIGN KEY(department_id) 
+REFERENCES department(id) 
+ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
+    manager_id INT,
+
+    FOREIGN KEY(role_id) 
+    REFERENCES role(id) 
+    ON DELETE SET NULL
 );
--- index.js
+++ index.js
@@ -3,23 +3,26 @@ const inquirer = require('inquirer');
 
 
 // Create an array for the questions to be stored in and prompted towards the user
-questions = [
-    {
-        type: "list",
-        name: "choices",
-        message: "What would you like to do?",
-        choices: [
-        "View All Employees", 
-        "Add Employee", 
-        "Update Employee Role", 
-        "View All Roles", 
-        "Add Role",  
-        "View All Department",
-        "Add Department",
-        "Quit"
+const generatePrompts = () => {
+    questions = [
+        {
+            type: "list",
+            name: "choices",
+            message: "What would you like to do?",
+            choices: [
+            "View All Employees", 
+            "Add Employee", 
+            "Update Employee Role", 
+            "View All Roles", 
+            "Add Role",  
+            "View All Department",
+            "Add Department",
+            "Quit"
+        ]
+        },
     ]
-    },
-]
+    return inquirer.prompt(questions);
+}
 
 
 // a function that Inits the app
@@ -27,8 +30,8 @@ function inits() {
     console.log("┌──────────────────┐");
     console.log("│ Employee Manager │");
     console.log("└──────────────────┘");
-    
-    inquirer.prompt(questions);
+
+    generatePrompts();
 };
 
 
