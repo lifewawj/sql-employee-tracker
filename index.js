0fb816a4708a7dd1af424dd6683cf554106fd5b3
lifewawj <arevaloaj01@gmail.com>
2024-01-09 00:33:41 -0800

    Imported inquirer, ADDED a questions object, Added a function to init the app

--- index.js
+++ index.js
@@ -5,6 +5,32 @@ const inquirer = require('inquirer');
 // Create an array for the questions to be stored in and prompted towards the user
 questions = [
     {
-        
-    }
+        type: "list",
+        name: "choices",
+        message: "What would you like to do?",
+        choices: [
+        "View All Employees", 
+        "Add Employee", 
+        "Update Employee Role", 
+        "View All Roles", 
+        "Add Role",  
+        "View All Department",
+        "Add Department",
+        "Quit"
+    ]
+    },
 ]
+
+
+// a function that Inits the app
+function inits() {
+    console.log("┌──────────────────┐");
+    console.log("│ Employee Manager │");
+    console.log("└──────────────────┘");
+    
+    inquirer.prompt(questions);
+};
+
+
+// Inits the app
+inits();
