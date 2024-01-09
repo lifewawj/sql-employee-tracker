e6f124fdb43cbd9448b3a8b2cec7a889d831676f
lifewawj <arevaloaj01@gmail.com>
2024-01-08 22:30:49 -0800

    ADDED an index.js file

---
+++ index.js
@@ -0,0 +1,10 @@
+// NPM Packages needed
+const inquirer = require('inquirer');
+
+
+// Create an array for the questions to be stored in and prompted towards the user
+questions = [
+    {
+        
+    }
+]
