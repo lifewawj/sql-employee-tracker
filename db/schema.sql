1caff534359f43eccb60b1c01b8566397846a0dd
lifewawj <arevaloaj01@gmail.com>
2024-01-09 16:30:30 -0800

    Updated Tables in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -9,29 +9,33 @@ USE employee_db;
 
 -- Tables created for department, role, employees
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30),
     department_id INT,
     salary DECIMAL,
 
-FOREIGN KEY(department_id) 
-REFERENCES department(id) 
-ON DELETE SET NULL
+    PRIMARY KEY(id),
+
+    FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
 
-    FOREIGN KEY(role_id) 
-    REFERENCES role(id) 
-    ON DELETE SET NULL
+    PRIMARY KEY(id),
+
+    FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE SET NULL,
+
+    FOREIGN KEY(id) REFERENCES employee(manager_id) ON DELETE SET NULL
 );
